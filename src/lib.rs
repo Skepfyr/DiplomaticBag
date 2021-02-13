1050b968d4d9176b5033247f2c53f5f1b816b475
Jack Rickard <jack.rickard@outlook.com>
2021-02-13 20:19:09 +0000

    Release version 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -31,7 +31,7 @@
 //! handy when dealing with FFI types that must remain on the same thread for
 //! their existence.
 
-#![doc(html_root_url = "https://docs.rs/diplomatic-bag/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/diplomatic-bag/0.1.1")]
 #![warn(
     keyword_idents,
     missing_crate_level_docs,
